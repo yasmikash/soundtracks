e026f310e862a64882818a3d3cc97b4c2c300516
Yasmika Saubhagya <yasmikash@yasmikash.local>
2025-10-04 11:31:48 +0530

    updates +++

---
+++ open.py
@@ -0,0 +1,39 @@
+from stable_audio_tools import get_pretrained_model
+from stable_audio_tools.inference.generation import generate_diffusion_cond
+import torch
+from einops import rearrange
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model, model_config = get_pretrained_model("stabilityai/stable-audio-open-1.0")
+model = model.to(device)
+
+conditioning = [
+    {"prompt": "soft ambient synth background", "seconds_start": 0, "seconds_total": 20}
+]
+
+output = generate_diffusion_cond(
+    model,
+    steps=100,
+    cfg_scale=7,
+    conditioning=conditioning,
+    sample_size=model_config["sample_size"],
+    sigma_min=0.3,
+    sigma_max=500,
+    sampler_type="dpmpp-3m-sde",
+    device=device,
+)
+
+output = rearrange(output, "b d n -> d (b n)")
+# Normalize and save
+import torchaudio
+
+output = (
+    output.to(torch.float32)
+    .div(torch.max(torch.abs(output)))
+    .clamp(-1, 1)
+    .mul(32767)
+    .to(torch.int16)
+    .cpu()
+)
+torchaudio.save("out.wav", output, model_config["sample_rate"])
